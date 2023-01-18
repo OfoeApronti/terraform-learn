116232492fd5c937abf10527e8819d4a5d467589
ofoe.apronti <ofoe.apronti@gmail.com>
2023-01-18 05:11:36 +0000

    creating a complete infra demo

--- main.tf
+++ main.tf
@@ -1,46 +1,54 @@
 provider "aws" {
-  region = "us-east-1"
-}
-variable "environment" {
-  description = "development environment"
-}
-
-variable "subnet_cidr_block" {
-  description = "subnet cidr block"
-  default = "10.0.10.0/24"
-  type = string
+  region="us-east-1"
 }
 
 variable "vpc_cidr_block" {
-  description= "vpc cidr block"
 }
 
-resource "aws_vpc" "dev-vpc" {
+variable "subnet_cidr_block" {
+}
+variable "avail_zone" {}
+variable "env_prefix" {
+  
+}
+
+
+
+resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name: "development",
-    vpc_end: "dev"
+    Name: "${var.env_prefix}-vpc"
   }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
-  vpc_id = aws_vpc.dev-vpc.id
-  //cidr_block="10.0.10.0/24"
+  vpc_id = aws_vpc.myapp-vpc.id
   cidr_block=var.subnet_cidr_block
-  availability_zone = "us-east-1a"
+  availability_zone = var.avail_zone
   tags = {
-    Name: "subnet-1-dev"
+    Name: "${var.env_prefix}-subnet-1"
   }
 }
 
-/*updating existing vpc or deployment
-data "aws_vpc" "existing_vpc" {
-  default = true
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags={
+    Name: "${var.env_prefix}-igw"
+  }
 }
 
-resource "aws_subnet" "dev-subnet-2" {
-  vpc_id = data.aws_vpc.existing_vpc.id
-  cidr_block="10.0.10.0/24"
-  availability_zone = "us-east-1a"
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags={
+    Name: "${var.env_prefix}-rtb"
+  }
 }
-*/ 
+
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id=aws_subnet.dev-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
