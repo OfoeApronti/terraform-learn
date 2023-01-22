1bc74291a0b722209b0a8f1a9f76646c9b1483b0
ofoe.apronti <ofoe.apronti@gmail.com>
2023-01-22 11:42:40 +0000

    terraform with defaults route table

--- main.tf
+++ main.tf
@@ -2,13 +2,12 @@ provider "aws" {
   region="us-east-1"
 }
 
-variable "vpc_cidr_block" {
-}
+variable "vpc_cidr_block" {}
 
-variable "subnet_cidr_block" {
-}
+variable "subnet_cidr_block" {}
 variable "avail_zone" {}
-variable "env_prefix" {
+variable "env_prefix" {}
+variable "my_ip" {
   
 }
 
@@ -36,7 +35,7 @@ resource "aws_internet_gateway" "myapp-igw" {
     Name: "${var.env_prefix}-igw"
   }
 }
-
+/*
 resource "aws_route_table" "myapp-route-table" {
   vpc_id = aws_vpc.myapp-vpc.id
   route {
@@ -51,4 +50,48 @@ resource "aws_route_table" "myapp-route-table" {
 resource "aws_route_table_association" "a-rtb-subnet" {
   subnet_id=aws_subnet.dev-subnet-1.id
   route_table_id = aws_route_table.myapp-route-table.id
+}
+*/
+
+//a default route table is created for the vpc. The subnet just created is associated with the default route table
+
+resource "aws_default_route_table" "default-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags={
+    Name: "${var.env_prefix}-default-rtb"
+  }
+}
+
+
+resource "aws_security_group" "myapp-sg" {
+  name="myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  //allows internet to port 8080 
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  //allows any traffic from the box to the internet
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+  tags={
+    Name: "${var.env_prefix}-sg"
+  }
 }
