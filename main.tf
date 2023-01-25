b87cf84d8109e6babbe65563aa5cb6b2cfbe8970
ofoe.apronti <ofoe.apronti@gmail.com>
2023-01-25 22:02:43 +0000

    added more steps

--- main.tf
+++ main.tf
@@ -7,7 +7,11 @@ variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
-variable "my_ip" {
+variable "my_ip" {}
+variable "instance_type" {
+  
+}
+variable "public_key_location" {
   
 }
 
@@ -93,5 +97,95 @@ resource "aws_security_group" "myapp-sg" {
   }
   tags={
     Name: "${var.env_prefix}-sg"
+  }
+}
+
+
+
+//using the default security group
+/*
+resource "aws_default_security_group" "default-sg" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  //allows internet to port 8080 
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  //allows any traffic from the box to the internet
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+  tags={
+    Name: "${var.env_prefix}-default-sg"
+  }
+}
+*/
+
+//to search for images 
+//go to  https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#Images:
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["amzn2*gp2"]
+
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+  public_key = file(var.public_key_location)
+}
+
+resource "aws_instance" "myapp-server" {
+  ami=data.aws_ami.latest-amazon-linux-image.id 
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.dev-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+  availability_zone = var.avail_zone
+  associate_public_ip_address = true
+  key_name = aws_key_pair.ssh-key.key_name
+
+  # user_data = <<EOF
+  #               #!/bin/bash
+  #               sudo yum update -y && sudo yum install -y docker
+  #               sudo systemctl start docker
+  #               sudo usermod -aG docker ec2-user
+  #               docker run -p 8080:80 nginx
+  #             EOF
+
+  user_data = file("entry-script.sh")
+  tags={
+    Name: "${var.env_prefix}-dev-server"
   }
 }
